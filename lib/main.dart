7911ad7e818c32dcca9c768c799cb9960707763e
kelvinwieth <wiethkelvin@gmail.com>
2024-02-04 14:31:55 -0300

    feat(options): add options

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,8 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 import 'package:necrologium/navigation/navigation.dart';
 import 'package:necrologium/shared/ui/styles/ne_colors.dart';
-import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
-  tz.initializeTimeZones();
   Intl.defaultLocale = 'pt_BR';
   await initializeDateFormatting();
   runApp(
--- lib/options/ui/options_screen.dart
+++ lib/options/ui/options_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 import 'package:necrologium/shared/ui/extensions/context_colors_helper.dart';
 import 'package:necrologium/shared/ui/texts/ne_texts.dart';
+import 'package:necrologium/shared/ui/widgets/spacers.dart';
 
 class OptionsScreen extends StatelessWidget {
   const OptionsScreen({super.key});
@@ -9,11 +11,169 @@ class OptionsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.colors.background,
-      body: Center(
-        child: NeDisplayText(
-          'Opcoes',
-          color: context.colors.onBackground,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              NeH1Text.bold(
+                'Opções',
+                color: context.colors.onBackground,
+              ),
+              const VerticalSpace(32),
+              Container(
+                decoration: BoxDecoration(
+                  color: context.colors.surfaceVariant,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 24,
+                ),
+                child: const Column(
+                  children: [
+                    _DarkModeOption(),
+                    Divider(height: 32),
+                    _ExportDataOption(),
+                    Divider(height: 32),
+                    _ClearDataOption(),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              const _RateUsButton(),
+              const VerticalSpace(40),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class _DarkModeOption extends StatelessWidget {
+  const _DarkModeOption();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          Icons.dark_mode_outlined,
+          color: context.colors.onSurfaceVariant,
+        ),
+        const HorizontalSpace(8),
+        Text(
+          'Modo Escuro',
+          style: TextStyle(
+            fontSize: 16,
+            color: context.colors.onSurfaceVariant,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        const Spacer(),
+        FlutterSwitch(
+          value: true,
+          onToggle: (_) {},
+          height: 24,
+          width: 48,
+          padding: 2,
+          activeColor: context.colors.primary,
+        ),
+      ],
+    );
+  }
+}
+
+class _ExportDataOption extends StatelessWidget {
+  const _ExportDataOption();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          Icons.upload_file,
+          color: context.colors.onSurfaceVariant,
+        ),
+        const HorizontalSpace(8),
+        Text(
+          'Exportar dados',
+          style: TextStyle(
+            fontSize: 16,
+            color: context.colors.onSurfaceVariant,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ClearDataOption extends StatelessWidget {
+  const _ClearDataOption();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          Icons.delete_outline,
+          color: context.colors.onSurfaceVariant,
+        ),
+        const HorizontalSpace(8),
+        Text(
+          'Limpar todos os dados',
+          style: TextStyle(
+            fontSize: 16,
+            color: context.colors.onSurfaceVariant,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _RateUsButton extends StatelessWidget {
+  const _RateUsButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: context.colors.surface,
+        borderRadius: BorderRadius.circular(8),
+        border: Border.all(
+          color: context.colors.primary.withOpacity(0.48),
+          width: 2,
+        ),
+      ),
+      padding: const EdgeInsets.all(12),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(
+            Icons.star,
+            size: 16,
+            color: context.colors.primary,
+          ),
+          const HorizontalSpace(8),
+          Text(
+            'Nos ajude com uma',
+            style: TextStyle(
+              color: context.colors.onSurface,
+            ),
+          ),
+          Text(
+            ' avaliação na loja',
+            style: TextStyle(
+              color: context.colors.primary,
+            ),
+          ),
+        ],
       ),
     );
   }
